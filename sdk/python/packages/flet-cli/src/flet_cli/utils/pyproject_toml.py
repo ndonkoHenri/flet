829c8e80859615b3d69215bd7b2dffd08af18102
ndonkoHenri <robotcoder4@protonmail.com>
2025-02-17 16:03:09 +0100

    reformat `get_pyproject` cli util

--- sdk/python/packages/flet-cli/src/flet_cli/utils/pyproject_toml.py
+++ sdk/python/packages/flet-cli/src/flet_cli/utils/pyproject_toml.py
@@ -1,22 +1,54 @@
 from pathlib import Path
-from typing import Any, Optional
+from typing import Any, Optional, Sequence, Union
 
 import toml
 
 
 def load_pyproject_toml(project_dir: Path):
+    """Load a pyproject.toml file and returns a function to retrieve configuration values."""
     pyproject_toml: Optional[dict[str, Any]] = {}
     pyproject_toml_file = project_dir.joinpath("pyproject.toml")
+
     if pyproject_toml_file.exists():
         with pyproject_toml_file.open("r") as f:
             pyproject_toml = toml.loads(f.read())
 
-    def get_pyproject(setting: str):
+    def get_pyproject_value(key: str):
+        """Retrieve a configuration value from pyproject.toml using a."""
         d = pyproject_toml
-        for k in setting.split("."):
-            d = d.get(k)
+        for i in key.split("."):
+            d = d.get(i)
             if d is None:
                 return None
         return d
+
+    def get_pyproject(keys: Union[str, Sequence[str]], option=None, default=None):
+        """
+        Retrieve a configuration value from either the CLI option or pyproject.toml settings.
+
+        Parameters:
+            *keys (str): One or more keys to look up in pyproject.toml (checked in order).
+            option (Any, optional): The value provided by the CLI build/package command. Defaults to None.
+            default (Any, optional): The fallback value if no valid setting is found. Defaults to None.
+
+        Returns:
+            Any: The first non-None value from `option`, pyproject.toml, or `default`.
+        """
+        # If CLI option is provided, return it immediately
+        if option is not None:
+            return option
+
+        # If a single key is provided, convert it to a list
+        if isinstance(keys, str):
+            keys = [keys]
+
+        # Lookup key in pyproject.toml and return the first non-None value
+        for key in keys:
+            value = get_pyproject_value(key)
+            if value is not None:
+                return value
+
+        # Fallback to default value
+        return default
 
     return get_pyproject
